2dd1d77e2d6a3b831e6a5fff5d1fb08e9efca6bd
Matt Distefano <eponymous@mattdistefano.com>
2017-07-31 13:43:57 -0700

    Clean up comments

--- webpack.config.js
+++ webpack.config.js
@@ -12,15 +12,10 @@ module.exports = {
     libraryTarget: 'umd',
     publicPath: '/'
   },
-
-  // Enable sourcemaps for debugging webpack's output.
   devtool: 'source-map',
-
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
-
   module: {
     rules: [
       {
@@ -64,13 +59,10 @@ module.exports = {
           ]
         })
       },
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader'
       },
-
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: 'pre',
         test: /\.js$/,
@@ -89,16 +81,6 @@ module.exports = {
       dataPath: 'data'
     })
   ],
-
-  // TODO use DLLs here
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  // externals: {
-  //     'react': 'React',
-  //     'react-dom': 'ReactDOM'
-  // },
   devServer: {
     historyApiFallback: true
   }
