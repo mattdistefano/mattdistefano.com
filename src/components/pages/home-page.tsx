dbdbad70125b5b914275431c7369612a536baa60
Matt Distefano <eponymous@mattdistefano.com>
2017-08-02 20:10:12 -0700

    Update home page

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { IndexPage, AsyncData } from '../../models';
+import { PageContentComponent } from '../page-content';
 import { PageCardListComponent } from '../page-card-list';
 
 export interface HomePageProps {
@@ -7,14 +8,17 @@ export interface HomePageProps {
 }
 
 export const HomePageComponent = (props: HomePageProps) => {
+  const page = props.page.data;
+
   const blogPosts =
-    props.page.data &&
-    props.page.data.children.find(child => child.path === '/blog/index');
+    page &&
+    page.children &&
+    page.children.find(child => child.path === '/blog/index');
 
   return (
     <div className="home-page">
-      <h1 className="sr-only">mattdistefano.com home</h1>
-      <h2 className="home-page__heading h1">Recent blog posts...</h2>
+      <PageContentComponent page={page} hideDate={true} />
+      <h2 className="h3">Recent blog posts...</h2>
       <PageCardListComponent pages={blogPosts && blogPosts.pages} />
     </div>
   );
