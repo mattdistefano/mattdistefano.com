de23cf68e73d4627001ff3b0d0d85a058a1501af
Matt Distefano <eponymous@mattdistefano.com>
2017-08-17 21:31:39 -0700

    Add content to standard index page

--- src/components/pages/standard-index-page.tsx
+++ src/components/pages/standard-index-page.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import { IndexPage, AsyncData } from '../../models';
-import { PageCardListComponent } from '../content';
+import { PageCardListComponent, PageContentComponent } from '../content';
 
 export interface StandardIndexPageProps {
   page?: AsyncData<IndexPage>;
 }
 
 // tslint:disable-next-line:variable-name
-export const StandardIndexPageComponent = (props: StandardIndexPageProps) =>
-  <div className="standard-index-page">
-    <PageCardListComponent
-      pages={props.page && props.page.data && props.page.data.pages}
-    />
-  </div>;
+export const StandardIndexPageComponent = (props: StandardIndexPageProps) => {
+  const page = props.page && props.page.data;
+
+  if (!page) {
+    return <div>Loading!</div>;
+  }
+
+  return (
+    <div className="standard-index-page">
+      <PageContentComponent title={page.titleHtml} content={page.content} />
+      <PageCardListComponent
+        pages={page.pages}
+      />
+    </div>
+  );
+};
